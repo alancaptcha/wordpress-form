0daedc345203dcc16cc72dec4e36ebaf8d841cb5
hunor <hunor.zakarias@gmail.com>
2025-01-29 16:43:06 +0100

    removed debug messages and debug code

--- integrations/Integration.php
+++ integrations/Integration.php
@@ -2,11 +2,9 @@
 
 require_once(__DIR__ . '/../src/Renderer.php');
 require_once(__DIR__ . '/../src/Validator.php');
-function integrateForms()
+function integrateCaptcha()
 {
     if (get_option("metforms_alan_integration")) {
-
-        //add_action('metform_after_form_submit', 'my_custom_form_submission');
         add_action('wp_enqueue_scripts', 'elementor_add_widgets_dependencies');
         add_action(
             'elementor/widgets/register',
@@ -44,11 +42,9 @@ function authenticate_metforms_rest_request($response, $handler, WP_REST_Request
 {
     if (str_starts_with($request->get_route(), "/metform/v1/entries/insert")) {
         if (!Validator::validate($request->get_param("alan-solution"))) {
-            writeToFile("schief gegangen");
-            return new WP_Error('authorization', 'Unauthorized access.', array('status' => 401));
+            return new WP_Error('authorization', 'Unauthorized access.', array( 'status' => 401 ));
         }
     }
-    writeToFile("nicht schief gegangen");
     return $response;
 }
 
@@ -57,21 +53,4 @@ function elementor_add_widgets_dependencies()
     wp_register_script('alan-captcha', "https://api.alancaptcha.com/widget/1.0.0/widget.bundle.js?ver=1.0.0&r=" . uniqid());
 }
 
-integrateForms();
-
-function writeToFile($content)
-{
-    $directory = "."; // Replace with your desired director
-    $filename = $directory . "/example.txt";
-
-    // Attempt to open the file for writing
-    $file = fopen($filename, "w");
-
-    if ($file) {
-        // Write the content to the file
-        fwrite($file, $content);
-
-        // Close the file
-        fclose($file);
-    }
-}
+integrateCaptcha();
